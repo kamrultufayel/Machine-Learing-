08ecb5c1bafcb6bcde35928074634e2f7dcc6d32
kamrultufayel <islam47249@gmail.com>
2023-10-19 14:21:07 +0100

    Read

---
+++ M1CW1.py
@@ -0,0 +1,10 @@
+#import data 
+
+
+import pandas as pd
+
+# Load the dataset
+data = pd.read_csv('cw1data.csv')
+
+# Display the first few rows of the dataset to inspect the data
+print(data.head())
